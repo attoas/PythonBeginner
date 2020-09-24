87ab45ab0d2527f02ddb813a19b92e95d1c4ab05
attoas <attoas@gmail.com>
2020-09-24 11:21:24 +0700

    lecture16.py

---
+++ FloatExample.py
@@ -0,0 +1,10 @@
+print(type(3.14))       # class 'float'
+print(type(1.5))        # class 'float'
+print(type(9.0))        # class 'float'
+print(type(-10.1))      # class 'float'
+print (10+3.14)
+print (type(10+3.14))
+print (10+3)
+print (type(10+3))
+print (10+3.0)
+print (type(10+3.0))
